84e0c3a30544958408d8fdb6b38bed76bc6165a5
DmitryYalchik-HOME <dmitry@ROFOCALE-PC>
2025-09-28 15:03:10 +0500

    Добавьте файлы проекта.

---
+++ DemoExam_09_02_07_02_2025_PU/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DemoExam_09_02_07_02_2025_PU
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
